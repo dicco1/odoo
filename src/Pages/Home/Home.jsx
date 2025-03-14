cad15a75bbf4d5fae755a242ee2666b3d8041f2b
Pardayev Diyorbek <pardayevdiyorbek2184@gmail.com>
2025-03-14 22:45:00 +0500

    Update Home.jsx

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -6,9 +6,9 @@ import { Swiper, SwiperSlide } from "swiper/react";
 
 import { Autoplay, FreeMode, Grid } from "swiper/modules";
 
-import Dars from "../../../public/darslar.jpg";
-import Tex from "../../../public/texnalog.jpg";
-import Komanda from "../../../public/komanda.jpg";
+import Dars from "/darslar.jpg";
+import Tex from "/texnalog.jpg";
+import Komanda from "/komanda.jpg";
 
 
 
@@ -18,7 +18,7 @@ const Home = () => {
   return (
     <div className="text-center container mx-auto">
       {/* Bosh qism (Hero Section) */}
-      <section className="px-44 py-20 m-0 text-blue-900 h-[100vh] bg-cover bg-center bg-[url('../../../public/back.jpg')]">
+      <section className="px-44 py-20 m-0 text-blue-900 h-[100vh] bg-cover bg-center bg-[url('/back.jpg')]">
         <div  className="relative  border-2 border-transparent mt-16 p-6 bg-white/30 rounded-lg h-[400px] shadow-lg backdrop-blur-md flex flex-col items-center py-32">
           <h2 className="text-5xl font-bold">
             Protech – Kelajak ilmini o‘rganing!
@@ -143,7 +143,7 @@ const Home = () => {
               {/* Image Section */}
               <div className="md:w-2/5 h-[489px] relative">
                 <img
-                  src="../../../public/FounderOfPdp-Img.693bb10fd8b0dda2661c.jpg"
+                  src="/FounderOfPdp-Img.693bb10fd8b0dda2661c.jpg"
                   alt="Professional in business attire"
                   className="w-full h-full object-cover"
                 />
@@ -207,7 +207,7 @@ maqsadlarimizga erishgan bo'lamiz.
           <div data-aos="fade-up" className="relative">
             <div className="w-[300px] h-[300px] lg:w-[400px] lg:h-[400px] rounded-full overflow-hidden border-8 border-teal-400">
               <img 
-                src="../../../public/rektor.0df6bf96ed6aa0ab7c1c.jpg" 
+                src="/rektor.0df6bf96ed6aa0ab7c1c.jpg" 
                 alt="Jemurat Mambetkarimov"
                 className="w-full h-full object-cover"
               />
@@ -265,7 +265,7 @@ maqsadlarimizga erishgan bo'lamiz.
         <div className="flex justify-center mb-6">
           <div className=" w-24 h-24 bg-gradient-to-br from-blue-400 to-purple-700 rounded-full flex items-center justify-center shadow-lg transform hover:scale-105 transition">
             <img
-              src="../../../public/Chat button.avif"
+              src="/Chat button.avif"
               alt="Online Support"
               className="w-20 h-20 object-cover opacity-80"
             />
