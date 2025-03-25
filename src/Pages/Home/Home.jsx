b39945b9a768eb6d38a319fd96608e6e6dbe1ccc
Pardayev Diyorbek <pardayevdiyorbek2184@gmail.com>
2025-03-25 15:24:17 +0500

    Update Home.jsx

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -219,7 +219,7 @@ function Home() {
               <p className="text-xl">Jemurat Mambetkarimov</p>
             </div>
               <div data-aos="fade-up" className="flex items-center  justify-center gap-24 mt-24">
-                <button className="hover:bg-transparent border transition-all duration-300 px-6 py-3 text-lg font-semibold text-blue-600 bg-white rounded-lg hover:bg-gray-200"><Link to={"/rector"}>Ko'proq Bilish</Link></button>
+                <button className="hover:bg-transparent border transition-all duration-300 px-6 py-3 text-lg font-semibold text-blue-600 bg-white rounded-lg hover:bg-gray-200"><Link to={"/about"}>Ko'proq Bilish</Link></button>
                 <button className="hover:bg-transparent border transition-all duration-300 px-6 py-3 text-lg font-semibold text-blue-600 bg-white rounded-lg hover:bg-gray-200"><Link to={"/register"}>Kursga Yozilish</Link></button>
               </div>
           </div>
